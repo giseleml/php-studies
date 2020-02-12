3d60f2c153f69490b5f3de55e944088f70316dfa
giseleml <giselemaraleo@gmail.com>
2020-02-11 21:09:45 -0300

    Superglobals

---
+++ basico/8_superglobals.php
@@ -0,0 +1,14 @@
+SUPERGLOBALS 
+
+Existem algumas variáveis pré-definidas no PHP, chamadas superglobals por serem sempre acessíveis 
+independente do escopo e é possível acessá-las em qualquer função, classe ou arquivo sem ter que fazer nada em especial.
+
+$GLOBALS
+$_SERVER
+$_REQUEST
+$_POST
+$_GET
+$_FILES
+$_ENV
+$_COOKIE
+$_SESSION
