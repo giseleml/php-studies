1826f6794a4a26f37736ca5ce8cfebdf0642516c
giseleml <giselemaraleo@gmail.com>
2020-02-11 21:05:31 -0300

    Operadores

---
+++ basico/7_operadores.php
@@ -0,0 +1,28 @@
+OPERADORES...
+
+> Aritméticos: + , -, *, /, %, **
+
+> Atribuidores: =, +=, -=, *=, /=, %=
+
+> Comparadores: ==, === e !== (Funcionam como em JS),  != e <> (diferente de), < , > , <= , >=
+
+> Incremento/Decremento: 
+    ++$num -> Pré-incremento, soma $num em um e o retorna 
+    $num++ -> Pós-incremento, retorna $num e então soma um 
+    --$num -> Pré-decremento 
+    $num-- -> Pós-decremento  
+
+> Lógicos: 
+    and/&& -> Retorna true se ambos comparativos forem true
+    or/||-> Retorna true se qualquer comparativo for true
+    xor -> Retorna true se qualquer comparativo for true, mas não os dois 
+    ! -> Retorna true se o comparativo não for true  (!false = true , !true = false)
+
+> Strings: 
+    . -> Concatenação ($texto1 . $texto2)
+    .= -> 
+
+> Condicionais: 
+
+    ?: -> Ternária 
+
