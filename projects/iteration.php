7957a6523cd1fcadc6f8b9119b1c4f63d1596e19
Gisele <42362130+giseleml@users.noreply.github.com>
2020-03-09 17:43:40 -0300

    Update iteration.php

--- projects/iteration.php
+++ projects/iteration.php
@@ -1,9 +1,11 @@
 <?php
 function printDevs() {
 	$people = array("Gisele", "Bill Gates", "Steve Jobs");
+	
 	$i  = 0;
+	echo "Programadores: <br>";
 	for ($i; $i < count($people); $i++) {
-		echo "Programadores: " . $people[$i] . "<br>";
+		echo $people[$i] . "<br>";
 	}
 }
 
